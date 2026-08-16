59ee42b78d6593e8e3026b4a23a3651b98824050
Nathan Osman <nathan@nathanosman.com>
2026-08-15 21:22:05 -0700

    Add Player and Manager classes for audio playback.

---
+++ src/manager.cpp
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2026 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "manager.hpp"
+
+Manager::~Manager()
+{
+    qDeleteAll(mPlayers);
+}
+
+Player *Manager::play(const QString &filename, float volume)
+{
+    Player *player;
+
+    if (mIdlePlayers.isEmpty()) {
+        player = new Player;
+        connect(player, &Player::finished, this, &Manager::onPlayerFinished);
+        connect(player, &Player::idleTimeout, this, &Manager::onPlayerIdleTimeout);
+        mPlayers.append(player);
+    } else {
+        player = mIdlePlayers.takeFirst();
+    }
+
+    player->play(filename, volume);
+    return player;
+}
+
+void Manager::stopAll()
+{
+    for (auto i : std::as_const(mPlayers)) {
+        i->stop();
+    }
+}
+
+void Manager::onPlayerFinished()
+{
+    Player *player = dynamic_cast<Player *>(sender());
+    mIdlePlayers.append(player);
+}
+
+void Manager::onPlayerIdleTimeout()
+{
+    Player *player = dynamic_cast<Player *>(sender());
+    mIdlePlayers.removeOne(player);
+    mPlayers.removeOne(player);
+    player->deleteLater();
+}
---
+++ src/player.hpp
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2026 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <QAudioOutput>
+#include <QMediaPlayer>
+#include <QTimer>
+
+class Player : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    Player();
+
+    void play(const QString &, float);
+    void stop();
+
+signals:
+
+    void finished();
+    void idleTimeout();
+
+private slots:
+
+    void onPlaybackStateChanged(QMediaPlayer::PlaybackState);
+
+private:
+
+    QAudioOutput mAudioOutput;
+    QMediaPlayer mMediaPlayer;
+    QTimer mTimer;
+};
+
+#endif // PLAYER_HPP
